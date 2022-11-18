aa0b87c392820a331b5ed4adb4858bf5d26e8169
gabriel96x1 <gabriel96x1@gmail.com>
2022-11-17 21:11:56 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Java CodeForces Template
+ 
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in) ;
+        int n = in.nextInt();
+        function(n);
+        in.close();
+    }
+
+    public static void function(int n) {
+        System.out.println(n);
+    }
+}
